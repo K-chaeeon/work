c6261f594177d6b35cc2571cd20cc79b19c9b623
K-chaeeon <searheh@gmail.com>
2024-07-05 14:14:19 +0900

    no message

--- java_wokr/Ex070501/src/ExAn1.java
+++ java_wokr/Ex070501/src/ExAn1.java
@@ -1,2 +1,22 @@
+//  1. 두 개의 주사위를 던졌을 때, 눈의 합이 6이 되는 모든 경우의 수를 출력하는 프로그램을 작성하시오.
+
+import java.sql.SQLOutput;
+
 public class ExAn1 {
+    public static void main(String[] args) {
+
+        int i = 1;
+        for ( ; i < 7; i++) ;
+        int j = 1;
+        for ( ; j < 7; j++) ;
+        int sum = 
+            if (i + j == 6); {
+                sum =
+                System.out.println(int i);
+            }
+
+
+    }
+
+
 }
