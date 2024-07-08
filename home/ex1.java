0ee5b482e7a94a16d7547ee301d1fc76ede9403d
K-chaeeon <searheh@gmail.com>
2024-07-08 21:13:45 +0900

    no message

--- home/ex1.java
+++ home/ex1.java
@@ -1,6 +1,4 @@
-import java.sql.SQLOutput;
-
-public class ex01 {
+public class ex1 {
     public static void main(String[] args) {
 
 //        int x = 4, y = 2;
---
+++ home/h0708/Hellojava.java
@@ -0,0 +1,14 @@
+package h0708;
+
+public class Hellojava {
+
+    public static void main(String[] args) {
+        int a;
+        System.out.println(a);
+
+
+
+    }
+
+
+}
