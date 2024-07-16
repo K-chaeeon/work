18c08470c1fbd02d536dde62d4f0932fc5954d44
K-chaeeon <searheh@gmail.com>
2024-07-16 15:25:40 +0900

    0716java

---
+++ java_wokr/ex0716/src/quiz/Qu01.java
@@ -0,0 +1,47 @@
+package quiz;
+
+/*
+1. "Hello.java" 문자열에서 파일명은 Hello와 확장자인 java를 분리시켜보자.
+
+    입력 >>Hello.java
+    출력
+    파일명 Hello
+    확장자 java
+
+Hello.java를 입력받음
+
+
+ */
+
+import java.util.Scanner;
+
+public class Qu01 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+
+        String str[] = new String[a.length()];
+
+        while (true) {
+
+            for (int i = 0; i < a.length(); i++) {
+
+                str[i] = String.valueOf(a.charAt(i));
+
+                System.out.println(str[i]);
+
+                if (str[i].equals('.')) ;
+                System.out.println(i);
+
+
+            }
+            break;
+
+        }
+    }
+}
+
+
+
+
---
+++ java_wokr/h0716/src/Q01.java
@@ -0,0 +1,45 @@
+/*
+public static void main(String[] args) {
+ int a = 1;
+ int b = 2;
+ int c = 3;
+ int sum = a + b + c;
+ double average = sum / 3.0;
+ System.out.println("평균값: " + average);
+ int x = 15;
+ int y = 25;
+ int z = 35;
+ sum = x + y + z;
+ average = sum / 3.0;
+ System.out.println("평균값: " + average);
+ }
+}
+위 코드를 리팩토링
+ */
+
+public class Q01 {
+
+    //    public static void doA(int a, int b, int c) {
+//        int sum = a + b + c;
+//        double average = sum / 3;
+//        System.out.println(average);
+    public static double average(int a, int b, int c) {
+        int sum = a + b + c;
+        return;
+        sum / 3.0;
+
+    }
+
+
+    public static void main(String[] args) {
+
+//        doA(1,2,3);
+//        System.out.println();
+//        doA(15,25,35);
+        System.out.println(average(1,2,3));
+        System.out.println(average(15,25,35));
+
+    }
+
+
+}
