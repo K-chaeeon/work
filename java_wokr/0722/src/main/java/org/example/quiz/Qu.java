8e568058a19334510d99eafa3d358811619374b5
K-chaeeon <searheh@gmail.com>
2024-07-22 13:39:34 +0900

    no message

--- java_wokr/0722/src/main/java/org/example/quiz/Qu.java
+++ java_wokr/0722/src/main/java/org/example/quiz/Qu.java
@@ -26,9 +26,35 @@ package org.example.quiz;
 */
 
 
+
 public class Qu {
 
+    class Rectangle{
+        public Rectangle(int x, int y, int width, int height){
+
+            int square = width * height;
+            void show
+
+        }
+
+    }
+
+    class Container {
+        boolean Contatins() {
 
 
+        }
+    }
 
+
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle(2, 2, 8, 7);
+        Rectangle s = new Rectangle(5, 5, 6, 6);
+        Rectangle t = new Rectangle(1, 1, 10, 10);
+
+        r.show();
+        System.out.println("s의 면적은 " + s.square());
+        if (t.contains(r)) System.out.println("t는 r을 포함합니다.");
+        if (t.contains(s)) System.out.println("t는 s를 포함합니다.");
+    }
 }
