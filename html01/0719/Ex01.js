bf0fc8c30aa1bb906b31c95c2067cf2a330ffa96
K-chaeeon <searheh@gmail.com>
2024-07-22 09:11:44 +0900

    no message

---
+++ html01/0719/Ex01.js
@@ -0,0 +1,59 @@
+
+//let-> 변경 가능한 변수, const->변경 불가능한 상수
+const aa = document.getElementById("btn-min");
+const bb = document.getElementById("btn-max");
+const printDiv = document.getElementById("print-div");
+
+const n1 = document.getElementById("num1");
+const n2 = document.getElementById("num2");
+const n3 = document.getElementById("num3");
+const n4 = document.getElementById("num4");
+const n5 = document.getElementById("num5");
+
+
+function min() {
+    const arr = [n1.value, n2.value, n3.value, n4.value, n5.value];
+    let min = arr[0];
+    for (let index = 0; index < arr.length; index++) {
+        const element = arr[index];
+        if (Number(min) > Number(element))
+            min = element;
+    }
+    printDiv.textContent = "최솟값 = " + min;
+}
+
+function max() {
+    const arr = [n1.value, n2.value, n3.value, n4.value, n5.value];
+    let max = arr[0];
+    for (const key of arr) {
+        if (Number(max) > Number(element))
+            max = key;
+    }
+    printDiv.textContent = "최댓값 = " + max;
+}
+
+
+aa.addEventListener('click', min);
+bb.addEventListener('click', max);
+
+
+printDiv.textContent = "내용을 넣어주세요";
+
+
+
+
+
+//js는 메인함수가 없음
+
+/*
+const cc = 10;
+cc = 20; //안됨 에러남
+
+let dd = 10;
+dd = 40; //가능
+*/
+
+// for(let i = 0; i<10; i++){
+//     console.log(i);
+// }
+
