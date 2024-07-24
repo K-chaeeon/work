66a4b321c04b68d80a0dac47f62a43b19ba001e2
K-chaeeon <searheh@gmail.com>
2024-07-24 12:09:57 +0900

    no message

--- java_wokr/0724/src/main/java/org/example/Ex04.java
+++ java_wokr/0724/src/main/java/org/example/Ex04.java
@@ -10,24 +10,32 @@ import static java.nio.file.Files.newBufferedWriter;
 
 public class Ex04 {
 
-    public static void md1() throws IOException {
-        Path file = Paths.get("d:\\simples.txt");
-        BufferedWriter writer = null;
+    public static void md1() {
+        Path file = Paths.get("simples.txt");
+//        BufferedWriter writer = null;
 
         //IOExeption이 발생하기 때문에 try catch로 하라고 뜸
-
-        writer = newBufferedWriter(file);
-        writer.write('A');
-        writer.write("문자열도 적어봅니다");
-
-        if (writer != null) ;
-        writer.close();
+        try (BufferedWriter writer = Files.newBufferedWriter(file)){
+            writer.write('A');
+            writer.write("문자열도 적어봅니다");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+//        finally {
+//            try {
+//                if (writer != null) ;
+//                writer.close();
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
     }
+
 
     public static void main(String[] args) {
         try {
             md1();
-        } catch (IOException e) {
+        } catch (Exception e) {
             System.out.println("파일만들기실패");
 //            e.printStackTrace;
         }
