fa58556430a78e992350ff0c06601ceb18a6b257
K-chaeeon <searheh@gmail.com>
2024-07-25 17:56:38 +0900

    no message

--- VStu/source/repos/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ VStu/source/repos/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -20,8 +20,47 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Console.WriteLine("출력");
-            label1.Text = "바뀜";
+            Random ran = new Random();
+
+            int num1 = ran.Next(47);
+            int num2 = ran.Next(47);
+            int num3 = ran.Next(47);
+            int num4 = ran.Next(47);
+            int num5 = ran.Next(47);
+            int num6 = ran.Next(47);
+           
+            Console.WriteLine("num1 = "+num1);
+            Console.WriteLine("num2 = "+num2);
+            Console.WriteLine("num3 = "+num3);
+            Console.WriteLine("num4 = "+num4);
+            Console.WriteLine("num5 = "+num5);
+            Console.WriteLine("num6 = "+num6);
+
+            label1.Text = num1.ToString();
+            label1.AutoSize = false;
+            label1.Size = new Size(139, 84);
+
+            label2.Text = num2.ToString();
+            label2.AutoSize = false;
+            label2.Size = new Size(139, 84);
+
+            label3.Text = num3.ToString();
+            label3.AutoSize = false;
+            label3.Size = new Size(139, 84);
+
+            label4.Text = num4.ToString();
+            label4.AutoSize = false;
+            label4.Size = new Size(139, 84);
+
+            label5.Text = num5.ToString();
+            label5.AutoSize = false;
+            label5.Size = new Size(139, 84);
+
+            label6.Text = num6.ToString();
+            label6.AutoSize = false;
+            label6.Size = new Size(139, 84);
+
+
         }
 
         private void label1_Click(object sender, EventArgs e)
