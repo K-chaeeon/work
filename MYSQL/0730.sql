630ab441c918db4ff1d104fd32bf028a961f575f
K-chaeeon <searheh@gmail.com>
2024-07-30 18:06:00 +0900

    0730 sql

--- MYSQL/0730.sql
+++ MYSQL/0730.sql
@@ -110,10 +110,133 @@ select custid as 아이디,
     
     
     -- 가격이 8천원 이상인 도서를 구매한 고객에 대하여 고객별 주문 도서의 총수량을 구하시오.    
-select custid as 아이디, count(*) as 주문수량 from Orders where saleprice>=8000 group by custid;
-
-
-
+    -- 단, 두 권 이상 구매한 고객만 출력
+select custid as 아이디, count(*) as 주문수량  from Orders where saleprice>=8000 group by custid having count(*)>=2;
+    -- (select~where) + from~ + group by~having
+    -- group by에서 쓰인 조건(속성)=select절에 쓰인 조건(속성)
+    
+    
+    
+		/*join*/
+    -- 고객과 고객의 주문에 관한 데이터 나타내기
+    select * from customer, orders 
+    where customer.custid=orders.custid		-- 중복되는 속성 합쳐서 출력
+    order by orders.orderid;	-- 뭘 기준으로 정렬할지 정하는 것
+    
+    
+    
+    -- 고객별로 주문한 모든 도서의 총판매액읠 구하고, 고객별로 정렬하시오.
+    select name, sum(saleprice)
+    from customer, orders
+    where customer.custid=orders.custid
+    group by customer.name
+    order by customer.name;
     
 
+-- 다시
+    -- 가격이 8천원 이상인 도서를 구매한 고객에 대하여 고객별 주문 도서의 총수량을 구하시오.    
+SELECT name AS 이름, COUNT(*) AS 주문수량
+FROM customer,orders
+where customer.custid=orders.custid
+GROUP BY customer.name
+HAVING COUNT(*) >= 2;
 
+
+		-- 고객의 이름과 고객이 주문한 도서의 이름,가격
+        -- 2만원 이상
+        select customer.name, book.bookname, book.price
+        from customer, orders, book
+        where book.bookid=orders.bookid and customer.custid=orders.custid and book.price>=20000
+        order by book.bookname;
+        
+
+
+
+-- 1) 도서번호가 1인 도서의 이름
+select book.bookid, book.bookname
+from book
+where bookid=1;
+
+-- 2) 가격이 2만원 이상인 도서의 이름
+select book.bookname, book.price
+from book
+where price>=20000;
+
+-- 3) 박지성의 총구매금액
+select sum(saleprice), customer.name
+from orders, customer
+where name='박지성' and orders.custid=customer.custid;
+
+-- 4) 박지성이 구매한 도서의 수
+select count(*), customer.name
+from customer, orders
+where orders.custid=customer.custid and customer.name='박지성';
+
+-- 5) 박지성이 구매한 도서의 출판사 수
+select count(distinct publisher), customer.name
+from book, customer, orders
+where customer.name='박지성' and orders.bookid=book.bookid and customer.custid=orders.custid;
+
+-- 6) 박지성이 구매한 도서의 이름, 가격, 정가와 판매가격의 차이
+select name, price, price-saleprice
+from book, customer, orders;
+
+
+
+-- 7) 박지성이 구매하지 않은 도서의 이름
+-- select distinct bookname
+-- from book, customer, orders
+-- where name not in ('박지성');
+
+
+-- 8) 마당서점 도서의 총개수
+select count(*) , book.bookid
+from book;
+
+-- 9) 마당서점에 도서를 납품하는 출판사의 총개수
+select count(distinct publisher) from book;
+
+-- 10) 모든 고객의 이름, 주소
+select customer.name, customer.adderss
+from customer;
+
+-- 11) 2024년 7월 4일부터 2024년 7월 7일 사이에 주문된 도서의 목록
+select book.bookname, orders.orderdata
+from orders, book
+where book.bookid=orders.bookid and orders.orderdata between'2024-07-04'and'2024-07-07';
+
+-- 12) 성이 김씨인 고객의 이름과 주소
+select name, adderss
+from customer
+where name like '김%%';
+
+-- 13) 성이 김씨이고 이름이 아로 끝나는 고객의 이름과 주소
+select name, adderss
+from customer
+where name like '김%_' and name like '_%아';
+
+
+-- 14) 주문 금액의 총액과  평균금액
+
+
+
+-- 15) 고객의 이름과 고객별 구매액
+
+
+-- 16) 고객의 이름과 고객이 구매한 도서목록
+
+
+
+	/*max*/
+    -- 가장 비싼 도서의 이름
+    select bookname from book
+    where price = (select max(price) from book);
+-- 서브쿼리 -> sql문 안에 또 다른 sql문이 들어가 있는 것alter
+
+-- 도서를 구매한 이력이 있는 고객명
+	select name from customer
+	where custid in (select custid from orders);
+
+	select distinct name 
+    from customer, orders
+	where orders.custid=customer.custid;
